31885a2490706719a9a57d21854b8e5ccd752f2a
Joan Pablo <johnpjm85@gmail.com>
2020-03-29 17:47:19 -0400

    commenting provider code

--- lib/providers/provider.dart
+++ lib/providers/provider.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_management_sample/providers/change_notifier_provider.dart';
 
+/// This class is responsible for providing the requested model
 class Provider {
+  /// Returns the nearest model up its widget tree
+  ///
+  /// If [rebuildContext] is `true` (default value), all the dependents widgets
+  /// will rebuild
+  ///
+  /// `rebuildContext: false` is necessary if want to avoid rebuilding the
+  /// [context] when model changes:
   static T of<T extends Listenable>(BuildContext context,
       {bool rebuildContext: true}) {
     if (rebuildContext) {
